0080ffa4ba76bcc48ddf687b0b8aa89031f29964
Jo-Sundaram <32679526+Jo-Sundaram@users.noreply.github.com>
2017-11-03 09:57:25 -0400

    Update Joke2.py

--- Joke2.py
+++ Joke2.py
@@ -1,4 +1,4 @@
-a = input("I knew a girl in highschool named 10k MegaOhm, she attracted everyone. (ask why))
+a = input("I knew a girl in highschool named 10k MegaOhm, everyone was attracted to her. (ask why))
 
 if a =='why':
   print("No one could resistor")
